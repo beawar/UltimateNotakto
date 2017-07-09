0cbb6c75a64abbc585af7013876ce043d3649fd4
Scramasax <scramasax443@gmail.com>
2017-07-09 20:15:06 +0200

    added no internet

--- Android/app/src/main/java/com/example/misterweeman/ultimatenotakto/activities/MainActivity.java
+++ Android/app/src/main/java/com/example/misterweeman/ultimatenotakto/activities/MainActivity.java
@@ -2,18 +2,21 @@ package com.example.misterweeman.ultimatenotakto.activities;
 
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
 import com.example.misterweeman.ultimatenotakto.App;
-import com.example.misterweeman.ultimatenotakto.services.MusicService;
 import com.example.misterweeman.ultimatenotakto.R;
 import com.example.misterweeman.ultimatenotakto.fragments.SignInFragment;
+import com.example.misterweeman.ultimatenotakto.services.MusicService;
 
 import static com.example.misterweeman.ultimatenotakto.preferences.Utility.loadLocale;
 
@@ -24,6 +27,7 @@ public class MainActivity extends AppCompatActivity{
     private boolean mIsBound = false;
     private MusicService mServ;
     private boolean firstTime = true;
+    private AlertDialog alertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -49,7 +53,6 @@ public class MainActivity extends AppCompatActivity{
         super.onResume();
         if(!firstTime) {
             mServ.resumeMusic();
-            firstTime=false;
         }
     }
 
@@ -57,18 +60,44 @@ public class MainActivity extends AppCompatActivity{
     public void goToNewGame(View view){
 
         Log.d(TAG, "goToNewGame()");
-
-        Intent intent = new Intent(this, GameActivity.class);
-        startActivity(intent);
+        if(isOnline()) {
+            Intent intent = new Intent(this, GameActivity.class);
+            startActivity(intent);
+        }else{
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(R.string.connection)
+                    .setTitle(R.string.connection_title)
+                    .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // do nothing: become a watcher
+                        }
+                    });
+            alertDialog = builder.create();
+            alertDialog.show();
+        }
     }
 
     // called when the user click "Statistiche"
     public void goToStats(View view){
+        if(isOnline()) {
+            Log.d(TAG, "goToStats()");
+            Intent intent = new Intent(this, GameStatsActivity.class);
+            startActivity(intent);
+        }else{
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(R.string.connection)
+                    .setTitle(R.string.connection_title)
+                    .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // do nothing: become a watcher
+                        }
+                    });
+            alertDialog = builder.create();
+            alertDialog.show();
 
-        Log.d(TAG, "goToStats()");
-
-        Intent intent = new Intent(this, GameStatsActivity.class);
-        startActivity(intent);
+        }
     }
 
     // called when the user click "Opzioni"
@@ -126,6 +155,14 @@ public class MainActivity extends AppCompatActivity{
             unbindService(Scon);
             mIsBound = false;
         }
+    }
+
+    public boolean isOnline() {
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return cm.getActiveNetworkInfo() != null &&
+                cm.getActiveNetworkInfo().isConnectedOrConnecting();
     }
 
 }
